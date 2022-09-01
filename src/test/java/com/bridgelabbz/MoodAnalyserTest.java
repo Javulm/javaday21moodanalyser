625f5bf364432c68609f21749b3ca4a71fc5f8b9
Javul Mulla <javulmulla8@gmail.com>
2022-09-01 16:46:13 +0530

    Added: When Happy or Any mood is passed, should return Happy mood functionality

--- src/test/java/com/bridgelabbz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabbz/MoodAnalyserTest.java
@@ -6,10 +6,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertSame;
 
 public class MoodAnalyserTest {
-@Test
-    public void givenSadMoodShouldReturnSad(){
-    MoodAnalyser analyse=new MoodAnalyser();
-    String mood=analyse.analyseMood("I am in sad mood");
-    assertSame("SAD", mood);
-}
+    @Test
+    public void givenSadMoodShouldReturnSad() {
+        MoodAnalyser analyse = new MoodAnalyser();
+        String mood = analyse.analyseMood("I am in sad mood");
+        assertSame("SAD", mood);
+    }
+
+    @Test
+    public void givenAnyMoodShouldReturnHappy() {
+        MoodAnalyser analyse = new MoodAnalyser();
+        String mood = analyse.analyseMood("I am in any mood");
+        assertSame("HAPPY", mood);
+    }
 }
