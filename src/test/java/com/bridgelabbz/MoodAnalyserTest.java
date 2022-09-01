c488335d9146cba4117cb27ec8a6be980a905b4c
Javul Mulla <javulmulla8@gmail.com>
2022-09-01 13:30:43 +0530

    Added: When sad mood is passed, should return SAD mood functionality

--- src/test/java/com/bridgelabbz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabbz/MoodAnalyserTest.java
@@ -1,5 +1,15 @@
 package com.bridgelabbz;
 
-public class MoodAnalyserTest {
+import com.bridgelabz.MoodAnalyser;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+public class MoodAnalyserTest {
+@Test
+    public void givenSadMoodShouldReturnSad(){
+    MoodAnalyser analyse=new MoodAnalyser();
+    String mood=analyse.analyseMood("I am in sad mood");
+    assertSame("SAD", mood);
+}
 }
